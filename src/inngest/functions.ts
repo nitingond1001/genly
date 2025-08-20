dd8436ad9d979e323f4b2c63e69fb85454822425
Nitin Gond <nitintechy80@gmail.com>
2025-08-20 14:59:40 +0530

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,19 @@
 import { openai, createAgent } from "@inngest/agent-kit";
-
+import { Sandbox } from "@e2b/code-interpreter";
 
 
 import { inngest } from "./client";
+import { stepsSchemas } from "inngest/api/schema";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("genly-nextjs-nitin-16");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-Agent",
       system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js and React snippets.",
@@ -18,6 +24,13 @@ export const helloWorld = inngest.createFunction(
         'Write the following snippet: ${event.data.value}',
     );
 
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return 'https://${host}';
+    })
+
+    return { output, sandboxUrl };
   },
 );
