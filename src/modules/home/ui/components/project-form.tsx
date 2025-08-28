39b21ab9ce2232c8114086d7cdb5d2c834d80607
Nitin Gond <nitintechy80@gmail.com>
2025-08-28 06:42:00 +0530

    16: authentication

--- src/modules/home/ui/components/project-form.tsx
+++ src/modules/home/ui/components/project-form.tsx
@@ -3,6 +3,7 @@
 import { z } from "zod";
 import { toast } from "sonner";
 import { useState } from "react";
+import { useClerk } from "@clerk/nextjs";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -25,6 +26,7 @@ const formSchema = z.object({
 export const ProjectForm = () => {
     const router = useRouter();
     const trpc = useTRPC();
+    const clerk = useClerk();
     const QueryClient = useQueryClient();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -42,8 +44,13 @@ export const ProjectForm = () => {
             // TODO: Invalidate usage status
         },
         onError: (error) => {
-            // TODO: Redirect to pricing page if specific error
             toast.error(error.message);
+            
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignUp();
+            }
+
+            // TODO: Redirect to pricing page if specific error
         },
     }));
 
